952f15a7ae0628e7242a43cf0e839dda6901f13b
kamalabai181 <kamalabai181@gmail.com>
2025-10-15 20:06:06 +0000

    Changes before Firebase Studio auto-run

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import MobileNav from '@/components/shared/MobileNav'
 import Sidebar from '@/components/shared/Sidebar'
 import { Toaster } from '@/components/ui/toaster'
+import Script from 'next/script'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -15,6 +16,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       </div>
       
       <Toaster />
+      {/* Add the Razorpay script to the layout */}
+      <Script src="https://checkout.razorpay.com/v1/checkout.js" />
     </main>
   )
 }
